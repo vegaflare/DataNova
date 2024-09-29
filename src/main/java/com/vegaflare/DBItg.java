d143153ae3b3d1a7e07fbaece23e41f09a76f1f9
SivaprasadPr <theallroundergaming@gmail.com>
2024-09-29 17:26:53 +0530

    minor addons for query generation

--- src/main/java/com/vegaflare/DBItg.java
+++ src/main/java/com/vegaflare/DBItg.java
@@ -3,6 +3,8 @@ package com.vegaflare;
 import com.vegaflare.utils.Logger;
 
 import java.sql.*;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAmount;
 
 
 public class DBItg {
@@ -29,6 +31,37 @@ public class DBItg {
         while (resultSet.next()) {
             System.out.println("Value:" + resultSet.getArray(1));
         }
+
+    }
+
+    // Function to generate and return query on the go
+    public String getQuery(String opc, int days, String archiveKeyValue, String status, String dbType, int client){
+        LocalDate lastday = LocalDate.now().minusDays(days);
+            // if opcode is deactivate
+            return "select eh_name, eh_ah_idnr"
+            +"from eh"
+            +"where eh_status in " + getStatusCode(status)
+            +" and eh_client = "+client
+            +"and to_char(eh_endtime, 'YYYYMMDD') < " + lastday
+            +"and eh_otype in ('JOBS','JOBP','JOBF','SCRI')"
+            +"order by"
+            +"case"
+            +"    when eh_starttype = '<JSCH>' then '001'"
+            +"    when eh_starttype = '<ONCE>' then '002'"
+            +"    when eh_starttype is null then '003'"
+            +"else eh_starttype"
+            +"        end;";
+
+    }
+
+    public String getStatusCode(String status){
+        switch (status){
+            case "ENDED_OK": return "= 1900";
+            case "ANY_OK"  : return "between 1900 and 1999";
+            case "ENDED_NOT_OK": return "= 1800";
+            case "ANY_ABEND"  : return "between 1800 and 1899";
+        }
+        return status;
     }
 
 }
